a32739f1e7a26743f26d402766b41e9e321cc684
Kausar Khizra <khizra6@gmail.com>
2018-08-18 12:56:25 -0700

    first commit of footer microservice

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <p>
+        Hello, this is is header (by header microservice)
+      </p>
+    )
+  }
+}
+
+
+export default App;
+
+
+
+
